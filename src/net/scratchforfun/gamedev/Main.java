02a07b23a66cd3a9db7101f494e9fcc140eb2c9f
ScratchForFun <magvaag@yahoo.no>
2014-09-23 19:50:23 +0200

    Main Class
    Episode 1

---
+++ src/net/scratchforfun/gamedev/Main.java
@@ -0,0 +1,30 @@
+package net.scratchforfun.gamedev;
+
+import javax.swing.*;
+
+/**
+ * Created by Scratch on 9/23/2014.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        // SwingUtilities.invokeLater();
+        new Main();
+    }
+
+    public Main(){
+        JFrame frame = new JFrame();
+        frame.setTitle("Game Development -by ScratchForFun");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Custom Icon
+        // Custom Cursor
+
+        // Screen
+
+        frame.setVisible(true);
+    }
+
+}
